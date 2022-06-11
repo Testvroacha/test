80e696219196ec952212523f134f28c4ae12467e
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2022-06-12 03:56:32 +0530

    Update info.py

--- spr/modules/info.py
+++ spr/modules/info.py
@@ -18,7 +18,7 @@ __HELP__ = """
 /info [CHAT_ID/Username|USER_ID/Username]
 
 or you can use inline mode >>
-@Anti_NsfwRobot [CHAT_ID/Username|USER_ID/Username]
+@SpamProtection_Bot [CHAT_ID/Username|USER_ID/Username]
 """
 
 
