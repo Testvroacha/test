7a6423efe4015c7a8d1cef4abdd5e17ea8845a1a
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2022-06-12 19:18:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,14 +15,14 @@ DEPLOYING_ON_HEROKU = (
 
 
 if not DEPLOYING_ON_HEROKU:
-    BOT_TOKEN = ""
-    SUDOERS = []
-    NSFW_LOG_CHANNEL = ""
-    SPAM_LOG_CHANNEL = ""
-    ARQ_API_KEY = ""  # Get it from @ARQRobot
+    BOT_TOKEN = "5557458280:AAFXz2_xxG_hhsYS3rXY2fTN6E9yXAKRMsQ"
+    SUDOERS = [5545068262]
+    NSFW_LOG_CHANNEL = "-1001667411233"
+    SPAM_LOG_CHANNEL = "-1001667411233"
+    ARQ_API_KEY = "GBGOGP-ZAUTAU-IJCFGH-GFDZQT-ARQ"  # Get it from @ARQRobot
 else:
-    BOT_TOKEN = env.get("BOT_TOKEN", "")
-    SUDOERS = [int(x) for x in env.get("SUDO_USERS_ID", "").split()]
-    NSFW_LOG_CHANNEL = int(env.get("NSFW_LOG_CHANNEL", ""))
-    SPAM_LOG_CHANNEL = int(env.get("SPAM_LOG_CHANNEL", ""))
-    ARQ_API_KEY = env.get("ARQ_API_KEY", "")
+    BOT_TOKEN = env.get("BOT_TOKEN", "5557458280:AAFXz2_xxG_hhsYS3rXY2fTN6E9yXAKRMsQ")
+    SUDOERS = [int(x) for x in env.get("SUDO_USERS_ID", "5545068262").split()]
+    NSFW_LOG_CHANNEL = int(env.get("NSFW_LOG_CHANNEL", "-1001667411233"))
+    SPAM_LOG_CHANNEL = int(env.get("SPAM_LOG_CHANNEL", "-1001667411233"))
+    ARQ_API_KEY = env.get("ARQ_API_KEY", "GBGOGP-ZAUTAU-IJCFGH-GFDZQT-ARQ")
