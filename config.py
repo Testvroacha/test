c117225d320d31d084fbadbb96d0e75f127b6af6
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2022-06-15 21:42:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ READ EVERYTHING CAREFULLY!!!
 
 
 DEPLOYING_ON_HEROKU = (
-    False  # Make this False if you're not deploying On heroku/Docker
+    True  # Make this False if you're not deploying On heroku/Docker
 )
 
 
@@ -23,6 +23,6 @@ if not DEPLOYING_ON_HEROKU:
 else:
     BOT_TOKEN = env.get("BOT_TOKEN", "5557458280:AAGqsLMWO9KcHOk2JJwZSlH7o-raX_GLH9Q")
     SUDOERS = [int(x) for x in env.get("SUDO_USERS_ID", "5545068262").split()]
-    NSFW_LOG_CHANNEL = int(env.get("NSFW_LOG_CHANNEL", "-1001690885542"))
+    NSFW_LOG_CHANNEL = int(env.get("NSFW_LOG_CHANNEL", "-1001667411233"))
     SPAM_LOG_CHANNEL = int(env.get("SPAM_LOG_CHANNEL", "-1001667411233"))
     ARQ_API_KEY = env.get("ARQ_API_KEY", "LQIHOH-OYUWTI-ZXHYNM-GDEBOB-ARQ")
