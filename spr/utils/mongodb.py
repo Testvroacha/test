28853a6bb4cd862c1efe5fb2aa363bc0a25b6a90
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2022-07-02 20:43:36 +0530

    Update mongodb.py

--- spr/utils/mongodb.py
+++ spr/utils/mongodb.py
@@ -5,7 +5,7 @@ from spr import MONGO_DB_URL as tmo
 MONGODB_CLI = Bot(tmo)
 db = MONGODB_CLI.program
 
-
+gbansdb = db.gban
 
 async def get_gbans_count() -> int:
     users = gbansdb.find({"user_id": {"$gt": 0}})
@@ -32,4 +32,29 @@ async def remove_gban_user(user_id: int):
     if not is_gbanned:
         return
     return await gbansdb.delete_one({"user_id": user_id})
+
+
+blacklist_chatdb = db.blacklistChat
+
+
+async def blacklisted_chats() -> list:
+    chats = blacklist_chatdb.find({"chat_id": {"$lt": 0}})
+    return [
+        chat["chat_id"] for chat in await chats.to_list(length=1000000000)
+    ]
+
+
+async def blacklist_chat(chat_id: int) -> bool:
+    if not await blacklist_chatdb.find_one({"chat_id": chat_id}):
+        await blacklist_chatdb.insert_one({"chat_id": chat_id})
+        return True
+    return False
+
+
+async def whitelist_chat(chat_id: int) -> bool:
+    if await blacklist_chatdb.find_one({"chat_id": chat_id}):
+        await blacklist_chatdb.delete_one({"chat_id": chat_id})
+        return True
+    return False
+
 
