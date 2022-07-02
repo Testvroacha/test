c09ffd70060f6aa25a95ba7ac8e5bee56d11eb31
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2022-07-03 00:18:59 +0530

    Update info.py

--- spr/modules/info.py
+++ spr/modules/info.py
@@ -3,7 +3,7 @@ from time import ctime
 from pyrogram import filters
 from pyrogram.types import (InlineQuery, InlineQueryResultArticle,
                             InputTextMessageContent, Message)
-
+from spr.utils.mongodb import get_served_users, is_served_user, add_served_user, get_served_chats, add_served_chat, remove_served_chat, is_served_chat, add_gban_user, is_gbanned_user, remove_gban_user, black_chat, blacklisted_chats, white_chat, is_black_chat
 from spr import SUDOERS, spr
 from spr.utils.db import (add_chat, add_user, chat_exists,
                           get_blacklist_event, get_nsfw_count,
@@ -27,13 +27,18 @@ async def get_user_info(user):
         user = await spr.get_users(user)
     except Exception:
         return
-    if not user_exists(user.id):
-        add_user(user.id)
+        if not user_exists(user.id):            
+            is_served = await is_served_user(user.id)
+        if not is_served:
+           add_user(user.id)
+           await add_served_user(user.id)
     trust = get_user_trust(user.id)
     blacklisted = is_user_blacklisted(user.id)
+    is_gbanned = await is_gbanned_user(user.id)
     reason = None
     if blacklisted:
-        reason, time = get_blacklist_event(user.id)
+        if is_gbanned:
+           reason, time = get_blacklist_event(user.id)
     data = f"""
 **ID:** {user.id}
 **DC:** {user.dc_id}
@@ -46,7 +51,7 @@ async def get_user_info(user):
 **Reputation:** {get_reputation(user.id)}
 **NSFW Count:** {get_nsfw_count(user.id)}
 **Potential Spammer:** {True if trust < 70 else False}
-**Blacklisted:** {blacklisted}
+**Blacklisted:** {is_gbanned}
 """
     data += (
         f"**Blacklist Reason:** {reason} | {ctime(time)}"
@@ -62,11 +67,15 @@ async def get_chat_info(chat):
     except Exception:
         return
     if not chat_exists(chat.id):
-        add_chat(chat.id)
+        if not await is_served_chat(chat.id):
+           add_chat(chat.id)
+           await add_served_chat(chat.id)           
     blacklisted = is_chat_blacklisted(chat.id)
+    blackchat = await is_black_chat(chat.id)
     reason = None
     if blacklisted:
-        reason, time = get_blacklist_event(chat.id)
+        if blackchat:
+           reason, time = get_blacklist_event(chat.id)
     data = f"""
 **ID:** {chat.id}
 **Username:** {chat.username}
@@ -74,7 +83,7 @@ async def get_chat_info(chat):
 **Members:** {chat.members_count}
 **Scam:** {chat.is_scam}
 **Restricted:** {chat.is_restricted}
-**Blacklisted:** {blacklisted}
+**Blacklisted:** {blackchat}
 """
     data += (
         f"**Blacklist Reason:** {reason} | {ctime(time)}"
