aaa46cf29978ac0f7ce8f603165283d3ae0df332
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2022-07-07 03:30:05 +0530

    Update watcher.py

--- spr/modules/watcher.py
+++ spr/modules/watcher.py
@@ -6,10 +6,9 @@ from pyrogram.types import Message
 from spr import SUDOERS, arq, spr
 from spr.utils.db import (add_chat, add_user, chat_exists,
                           is_chat_blacklisted, is_nsfw_downvoted,
-                          is_nsfw_enabled, is_spam_enabled,
                           is_user_blacklisted, update_spam_data,
                           user_exists)
-from spr.utils.mongodb import get_served_users, is_served_user, add_served_user, get_served_chats, add_served_chat, remove_served_chat, is_served_chat, add_gban_user, is_gbanned_user, remove_gban_user, black_chat, blacklisted_chats, white_chat, is_black_chat
+from spr.utils.mongodb import get_served_users, is_served_user, add_served_user, get_served_chats, add_served_chat, remove_served_chat, is_served_chat, add_gban_user, is_gbanned_user, remove_gban_user, black_chat, blacklisted_chats, white_chat, is_black_chat, is_nsfw_enabled, is_spam_enabled
 from spr.utils.functions import (delete_nsfw_notify,
                                  delete_spam_notify, kick_user_notify)
 from spr.utils.misc import admins, get_file_id, get_file_unique_id
@@ -70,6 +69,8 @@ async def message_watcher(_, message: Message):
         os.remove(file)
         if resp.ok:
             if resp.result.is_nsfw:
+                is_nsfw = await is_nsfw_enabled(chat_id)
+                if is_nsfw:
                     return await delete_nsfw_notify(
                         message, resp.result
                     )
@@ -82,6 +83,9 @@ async def message_watcher(_, message: Message):
         return
     result = resp.result[0]
     if not result.is_spam:
+        return
+    is_spam = await is_spam_enabled(chat_id)
+    if not is_spam:
         return
     if user_id in SUDOERS or user_id in (await admins(chat_id)):
         return
