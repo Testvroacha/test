e21130d7bba2485284333349a4f2cc4d9839311b
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2022-07-07 02:21:33 +0530

    Update functions.py

--- spr/utils/functions.py
+++ spr/utils/functions.py
@@ -93,8 +93,6 @@ __Message has been deleted__
 
     keyb = ikb(
         {
-            "Correct (0)": "upvote_spam",
-            "Incorrect (0)": "downvote_spam",
             "Chat": "https://t.me/" + (message.chat.username or "spamlogsss/11"),
         },
         2
