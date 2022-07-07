1d3f295906bc278f434d89a11d1855c52feacb85
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2022-07-07 22:41:27 +0530

    Update watcher.py

--- spr/modules/watcher.py
+++ spr/modules/watcher.py
@@ -7,7 +7,7 @@ from spr.utils.db import (add_chat, add_user, chat_exists,
                           is_chat_blacklisted, is_nsfw_downvoted,
                           is_user_blacklisted, update_spam_data,
                           user_exists)
-from spr.utils.mongodb import get_served_users, is_served_user, add_served_user, get_served_chats, add_served_chat, remove_served_chat, is_served_chat, add_gban_user, is_gbanned_user, remove_gban_user, black_chat, blacklisted_chats, white_chat, is_black_chat, is_nsfw_enabled, is_spam_enabled, disable_nsfw, disable_spam, enable_nsfw, enable_spam, is_arab_enabled
+from spr.utils.mongodb import get_served_users, is_served_user, add_served_user, get_served_chats, add_served_chat, remove_served_chat, is_served_chat, add_gban_user, is_gbanned_user, remove_gban_user, black_chat, blacklisted_chats, white_chat, is_black_chat, is_nsfw_enabled, is_spam_enabled, disable_nsfw, disable_spam, enable_nsfw, enable_spam, del_anti_func, set_anti_func, get_anti_func
 from spr.utils.functions import (delete_nsfw_notify,
                                  delete_spam_notify, kick_user_notify)
 from spr.utils.misc import admins, get_file_id, get_file_unique_id
