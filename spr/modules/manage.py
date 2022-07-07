d4500256c4b5abc02fd57bce58221a8f08d61f3a
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2022-07-07 23:04:46 +0530

    Update manage.py

--- spr/modules/manage.py
+++ spr/modules/manage.py
@@ -23,6 +23,14 @@ class REGEXES:
     arab = compile('[\u0627-\u064a]')
 
 
+def get_arg(message):
+    msg = message.text
+    msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
+    split = msg[1:].replace("\n", " \n").split(" ")
+    if " ".join(split[1:]).strip() == "":
+        return ""
+    return " ".join(split[1:])
+
 
 FORM_AND_REGEXES = {
     "ar": [REGEXES.arab, "arabic"],
