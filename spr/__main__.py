caf9cdf88531ff080e4f117205dac9a84a4185cc
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2022-07-10 18:23:56 +0530

    Update __main__.py

--- spr/__main__.py
+++ spr/__main__.py
@@ -6,7 +6,7 @@ from pyrogram import filters, idle
 from pyrogram.types import (CallbackQuery, InlineKeyboardButton,
                             InlineKeyboardMarkup, Message)
 
-from spr import BOT_USERNAME, session, spr, SPAM_LOG_CHANNEL
+from spr import BOT_USERNAME, session, spr
 from spr.core import ikb
 from spr.modules import MODULES
 from spr.utils.misc import paginate_modules
