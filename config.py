dbdf696512ce71fa19809fd0c3a3df1fb361d27c
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2022-07-10 13:33:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,14 +17,10 @@ DEPLOYING_ON_HEROKU = (
 if not DEPLOYING_ON_HEROKU:
     BOT_TOKEN = "5557458280:AAGqsLMWO9KcHOk2JJwZSlH7o-raX_GLH9Q"
     SUDOERS = [5545068262]
-    NSFW_LOG_CHANNEL = "-1001545810140"
-    SPAM_LOG_CHANNEL = "-1001545810140"
     ARQ_API_KEY = "SCQEDR-KAVRFC-SPYXEK-MVUKWI-ARQ"  # Get it from @ARQRobot
     MONGO_DB_URL = "mongodb+srv://spam:spam@cluster0.2vykm.mongodb.net/?retryWrites=true&w=majority"
 else:
     BOT_TOKEN = env.get("BOT_TOKEN", "5557458280:AAGqsLMWO9KcHOk2JJwZSlH7o-raX_GLH9Q")
     SUDOERS = [int(x) for x in env.get("SUDO_USERS_ID", "5545068262").split()]
-    NSFW_LOG_CHANNEL = int(env.get("NSFW_LOG_CHANNEL", "-1001545810140"))
-    SPAM_LOG_CHANNEL = int(env.get("SPAM_LOG_CHANNEL", "-1001545810140"))
     ARQ_API_KEY = env.get("ARQ_API_KEY", "SCQEDR-KAVRFC-SPYXEK-MVUKWI-ARQ")
     MONGO_DB_URL = env.get("MONGO_DB_URL", "mongodb+srv://spam:spam@cluster0.2vykm.mongodb.net/?retryWrites=true&w=majority")
