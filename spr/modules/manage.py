a510556fde800d7db7012360c9307a2315c1e31a
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2022-07-15 21:41:05 +0530

    Update manage.py

--- spr/modules/manage.py
+++ spr/modules/manage.py
@@ -163,7 +163,7 @@ async def nsfw_scan_command(_, message: Message):
         return await m.edit("Something went wrong.")
     file = await spr.download_media(file_id)
     try:
-        data = requests.post(f"https://api.safone.tech/nsfw", files={'image': open(file, 'rb')}).json()
+        data = requests.post(f"https://api.safone.tech/nsfw", json={'image': open(file, 'rb')}).json()
         is_nsfw = data['data']['is_nsfw']
         hentai = data['data']['hentai']
         drawings = data['data']['drawings']
@@ -175,7 +175,7 @@ async def nsfw_scan_command(_, message: Message):
     remove(file)
     await m.edit(
         f"""
-**Neutral:** `{neutral} %`
+**Neutral:** `{neutral}() %`
 **Porn:** `{porn} %`
 **Hentai:** `{hentai} %`
 **Sexy:** `{sexy} %`
