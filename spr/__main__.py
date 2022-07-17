bdba71461643ca619a9a1c4f7b138afc037d67c4
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2022-07-18 03:10:07 +0530

    Update __main__.py

--- spr/__main__.py
+++ spr/__main__.py
@@ -47,8 +47,8 @@ async def help_command(_, message: Message):
     kb = ikb(
         {
             {"Help": "bot_commands"},
-            "Add Me To Your Group": f"https://t.me/{BOT_USERNAME}?startgroup=new",
-            "Support Chat": "https://t.me/CheemsUserbot",
+            {"Add Me To Your Group": f"https://t.me/{BOT_USERNAME}?startgroup=new"},
+            {"Support Chat": "https://t.me/CheemsUserbot"},
         }
     )
     mention = message.from_user.mention
