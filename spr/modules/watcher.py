3ce3facaa5a88e157294dea1bc07bc71b494d768
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2022-07-23 02:47:16 +0530

    Update watcher.py

--- spr/modules/watcher.py
+++ spr/modules/watcher.py
@@ -78,9 +78,6 @@ async def message_watcher(_, message: Message):
     text = message.text or message.caption
     if not text:
         return
-    check = ad.detect_alphabet("{}".format(text))
-    if "ARABIC" in check:        
-        return await message.delete()
     try:
          resp = await api.spam_scan(text)
     except GenericApiError:
@@ -98,3 +95,38 @@ async def message_watcher(_, message: Message):
     if user_id in SUDOERS or user_id in (await admins(chat_id)):
         return
     await delete_spam_notify(message, datas)
+
+
+
+
+
+
+@spr.on_message(
+    (
+        filters.document
+        | filters.text
+    )
+)
+async def arab_watcher(_, message: Message):
+    user_id = None
+    chat_id = None
+
+    if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
+        chat_id = message.chat.id    
+    if message.from_user:
+        if message.from_user.id:
+            user_id = message.from_user.id
+    if not chat_id or not user_id:
+        return
+    text = message.text or message.caption
+    if not text:
+        return
+    check = ad.detect_alphabet("{}".format(text))
+    is_arab = await is_arab_enabled(chat_id)
+    if is_arab:
+       if "ARABIC" in check:
+            try:        
+               return await message.delete()
+            except Exception:
+                return
+    
