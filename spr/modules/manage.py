6c3cfaad0f26091de8c43d8fe99693463abff4ae
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2022-08-10 19:17:11 +0530

    Update manage.py

--- spr/modules/manage.py
+++ spr/modules/manage.py
@@ -13,15 +13,6 @@ __HELP__ = """
 /nsfwscan - Classify a media.
 """
 
-def get_arg(message):
-    msg = message.text
-    msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
-    split = msg[1:].replace("\n", " \n").split(" ")
-    if " ".join(split[1:]).strip() == "":
-        return ""
-    return " ".join(split[1:])
-
-
 @spr.on_message(
     filters.command("antinsfw") & ~filters.private, group=3
 )
