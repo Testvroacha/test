2ad6b60a626c56cdef1b69a32133d29505d4959e
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2022-08-11 00:51:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,11 +15,11 @@ DEPLOYING_ON_HEROKU = (
 
 
 if not DEPLOYING_ON_HEROKU:
-    BOT_TOKEN = "5557458280:AAFkAxIVVdew5IkXqRJ1VXn3QVkGZyRbw_o"
+    BOT_TOKEN = "5442868128:AAH18gxDfJny0VNkUjYp9S3Xi9X08ovaNmY"
     SUDOERS = [5545068262]
     MONGO_DB_URL = "mongodb+srv://spam:spam@cluster0.2vykm.mongodb.net/?retryWrites=true&w=majority"
 else:
-    BOT_TOKEN = env.get("BOT_TOKEN", "5557458280:AAFkAxIVVdew5IkXqRJ1VXn3QVkGZyRbw_o")
+    BOT_TOKEN = env.get("BOT_TOKEN", "5442868128:AAH18gxDfJny0VNkUjYp9S3Xi9X08ovaNmY")
     SUDOERS = [int(x) for x in env.get("SUDO_USERS_ID", "5545068262").split()]
     MONGO_DB_URL = env.get("MONGO_DB_URL", "mongodb+srv://spam:spam@cluster0.2vykm.mongodb.net/?retryWrites=true&w=majority")
   
