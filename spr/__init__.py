2fd10bf7a8f43e4ad2526e24088fd96fc5964a7d
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2022-08-10 19:00:48 +0530

    Update __init__.py

--- spr/__init__.py
+++ spr/__init__.py
@@ -2,16 +2,9 @@ from os.path import exists
 
 from aiohttp import ClientSession
 from pyrogram import Client
-from Python_ARQ import ARQ
-from SafoneAPI import SafoneAPI
-from alphabet_detector import AlphabetDetector
-
-api = SafoneAPI()
-ad = AlphabetDetector()
 
 API_ID = 6
 API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
-ARQ_API_URL = "https://arq.hamker.in"
 
 if exists("config.py"):
     from config import *
@@ -19,9 +12,6 @@ else:
     from sample_config import *
 
 session = ClientSession()
-
-
-arq = ARQ(ARQ_API_URL, ARQ_API_KEY, session)
 
 spr = Client(
     name="spr",
