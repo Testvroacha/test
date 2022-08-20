6a895ce0e65a97695e40c6ab93982a7006f078fd
Testvroacha <111535036+Testvroacha@users.noreply.github.com>
2022-08-21 03:49:41 +0530

    Update watcher.py

--- spr/modules/watcher.py
+++ spr/modules/watcher.py
@@ -3,7 +3,7 @@ from pyrogram import filters, enums
 from pyrogram.types import Message
 import opennsfw2 as n2
 from spr import SUDOERS, spr
-from spr.utils.mongodb import get_served_users, is_served_user, add_served_user, get_served_chats, add_served_chat, remove_served_chat, is_served_chat, add_gban_user, is_gbanned_user, remove_gban_user, black_chat, blacklisted_chats, white_chat, is_black_chat, is_nsfw_enabled, enable_nsfw, disable_nsfw
+from spr.utils.mongodb import get_served_users, is_served_user, add_served_user, get_served_chats, add_served_chat, remove_served_chat, is_served_chat, add_gban_user, is_gbanned_user, remove_gban_user, black_chat, blacklisted_chats, white_chat, is_black_chat, is_nsfw_enabled, enable_nsfw, disable_nsfw, is_admin_chat
 from spr.utils.functions import (delete_nsfw_notify, kick_user_notify)
 from spr.utils.misc import admins, get_file_id, get_file_unique_id
 
@@ -51,25 +51,28 @@ async def message_watcher(_, message: Message):
         is_serve = await is_served_chat(chat_id)
         if not is_serve:
                await add_served_chat(chat_id)
-        if user_id in SUDOERS or user_id in (await admins(chat_id)):
-            return
-        is_gbanned = await is_gbanned_user(user_id)                  
+        is_gbanned = await is_gbanned_user(user_id)                 
         if is_gbanned:
             await kick_user_notify(message)
         file = await spr.download_media(file_id)
         try:
-            A, results = n2.predict_video_frames(video_path=file, frame_interval=100000)            
+            A, results = n2.predict_video_frames(video_path=file, frame_interval=100000)                        
         except Exception:
             try:
                 return os.remove(file)
             except Exception:
                 return
         os.remove(file)     
-        result = round(max(results))
-        if result > 0:
+        hel = max(results)
+        result = format(hel, '.0%')
+        detected = result.replace("%", "")
+        if int(detected) > 10:
                 is_nfw = await is_nsfw_enabled(chat_id)
                 if is_nfw:
-                    return await delete_nsfw_notify(
-                        message, results
-                    )
+                         admin_chat = await is_admin_chat(chat_id)
+                         if not admin_chat:                             
+                            if user_id not in SUDOERS or user_id not in (await admins(chat_id)):
+                               return await delete_nsfw_notify(message, detected)
+                         else:               
+                            return await delete_nsfw_notify(message, detected)
     
