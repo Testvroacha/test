5fb5c50eeee8b1f6d93c0f09c6ff76022a826833
Testvroacha <111535036+Testvroacha@users.noreply.github.com>
2022-08-22 03:00:04 +0530

    Update info.py

--- spr/modules/info.py
+++ spr/modules/info.py
@@ -3,7 +3,7 @@ from time import ctime
 from pyrogram import filters
 from pyrogram.types import (InlineQuery, InlineQueryResultArticle,
                             InputTextMessageContent, Message)
-from spr.utils.mongodb import get_served_users, is_served_user, add_served_user, get_served_chats, add_served_chat, remove_served_chat, is_served_chat, add_gban_user, is_gbanned_user, remove_gban_user, black_chat, blacklisted_chats, white_chat, is_black_chat
+from spr.utils.mongodb import get_served_users, is_served_user, add_served_user, get_served_chats, add_served_chat, remove_served_chat, is_served_chat, add_gban_user, is_gbanned_user, remove_gban_user, black_chat, blacklisted_chats, white_chat, is_black_chat, get_nsfw_count
 from spr import SUDOERS, spr
 
 async def get_user_info(user):
@@ -15,6 +15,7 @@ async def get_user_info(user):
         if not is_serve:
            await add_served_user(user.id)
     is_gbanned = await is_gbanned_user(user.id)
+    nsfw_count = await get_nsfw_count(user.id)
     reason = None
     data = f"""
 **ID:** {user.id}
@@ -23,7 +24,9 @@ async def get_user_info(user):
 **Mention: ** {user.mention("Link")}
 
 **Is Sudo:** {user.id in SUDOERS}
-**Spammer:** {is_gbanned}
+**Trust:** {100 - nsfw_count * 10}
+**Spammer:** {True if nsfw_count > 10 else False}
+**NSFW Count:** {nsfw_count}
 **Blacklisted:** {is_gbanned}
 """
     return data
